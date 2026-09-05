4f7f8db3547d5a98e5c4d993b42dc6849951aff3
Advaith Manoj <88617390+advaithmanoj@users.noreply.github.com>
2026-09-05 12:09:17 +0530

    Create apb_slave_pkg.sv

---
+++ tb/apb_slave_pkg.sv
@@ -0,0 +1,9 @@
+package apb_slave_pkg;
+  `include "apb_slave_transaction.sv"
+  `include "apb_slave_generator.sv"
+  `include "apb_slave_driver.sv"
+  `include "apb_slave_monitor.sv"
+  `include "apb_slave_agent.sv"
+  `include "apb_slave_scoreboard.sv"
+  `include "apb_slave_environment.sv"
+endpackage
